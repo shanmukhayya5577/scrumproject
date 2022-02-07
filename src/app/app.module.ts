c8958986e2477134076614db0903453e78bba80f
saimighty9999 <saimighty9999@gmail.com>
2022-02-07 16:17:17 +0530

    login page -sai

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
+    ReactiveFormsModule,FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  loginForm=new FormGroup({
+    email:new FormControl("",[Validators.required,Validators.minLength(8)]),
+    pass: new FormControl("",[Validators.required,Validators.minLength(8)])
+  })
+  get email(){
+    return this.loginForm.get("email")
   }
+  get pass(){
+    return this.loginForm.get("pass")
+  }
+  login(){
+    console.log(this.loginForm.value);
+    
+  }
+  
+    constructor() { }
+  
+    ngOnInit(): void {
+    }
+  
 
 }
+
+
